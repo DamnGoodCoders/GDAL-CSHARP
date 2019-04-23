8d9b85aad37d069593631fc58d954014367d30b0
tzw <xymeth_planestrider@yahoo.com>
2019-04-22 18:24:57 -0600

    Minor Fixes

--- GPDF2GTIF/GPDF2GTIF/Form1.cs
+++ GPDF2GTIF/GPDF2GTIF/Form1.cs
@@ -12,6 +12,7 @@ namespace GPDF2GTIF
 {
     public partial class Form1 : Form
     {
+        GTranslate translate = new GTranslate();
         public Form1()
         {
             InitializeComponent();
@@ -19,27 +20,27 @@ namespace GPDF2GTIF
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            Program.CreateDir();
+            translate.CreateDir();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            Program.fPath = textBox1.Text;
+            translate.SetfPath(textBox1.Text);
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            Program.fName = textBox3.Text;
+            translate.SetfName(textBox3.Text);
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            Program.fOutputPath = textBox2.Text;
+            translate.SetfOutputPath(textBox2.Text);
         }
 
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            Program.fOutputFile = textBox4.Text;
+            translate.SetfOutputFilename(textBox4.Text);
         }
     }
 }
---
+++ GPDF2GTIF/GPDF2GTIF/GTranslate.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPDF2GTIF
+{
+    class GTranslate
+    {
+        private static String fPath;
+        private static String fName;
+        private static String fOutputPath;
+        private static String fOutputFileName;
+
+        // Getters and Setters
+        public String GetfName()
+        {
+            return fName;
+        }
+        public void SetfName(String a)
+        {
+            fName = @a;
+        }
+        public String GetfOutput()
+        {
+            return fOutputPath;
+        }
+        public void SetfOutputPath(String a)
+        {
+            fOutputPath = @a;
+        }
+        public String GetfPath()
+        {
+            return fPath;
+        }
+        public void SetfPath(String a)
+        {
+            fPath = @a;
+        }
+        public String GetfOutputFileName()
+        {
+            return fOutputFileName;
+        }
+        public void SetfOutputFilename(String a)
+        {
+            fOutputFileName = @a;
+        }
+        // Getters and Setters End
+
+        private static void CreatePro()
+        {
+            try
+            {
+                String cmdargs = "/C gdal_translate -of PDF " + fPath + "/" + fName + " " + fOutputPath + '/' + fOutputFileName;
+                Console.WriteLine("Path : " + @fPath + " File Name : " + @fName + "Output Destination: " + fOutputPath);
+                System.Diagnostics.Process.Start("CMD.exe", cmdargs);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error! : " + e.ToString());
+            }
+            finally
+            {
+
+            }
+        }
+
+        public void CreateDir()
+        {
+            try
+            {
+                if (!Directory.Exists(fPath))
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(fPath.ToString());
+                    DirectoryInfo dl = Directory.CreateDirectory(fOutputPath.ToString());
+                    CreatePro();
+                }
+                else
+                {
+                    Console.WriteLine("Directory Already Exists.");
+                    CreatePro();
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to Process Request : {0}", e.ToString());
+            }
+            finally
+            {
+
+            }
+        }
+
+        public GTranslate()
+        {
+            SetfPath(@"");
+            SetfName(@"");
+            SetfOutputPath(@"");
+            SetfOutputFilename(@"");
+        }
+    }
+}
+
